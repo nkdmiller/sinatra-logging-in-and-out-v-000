b7c3d0b81ed4e97547068a6da1582e1dc7c62181
Nate Miller <natekmiller96@gmail.com>
2018-08-23 22:11:50 +0000

    automatically backed up by learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
+<<<<<<< HEAD
   validates_presence_of :username, :balance, :password
+=======
+  validates_presence_of :name, :email, :password
+>>>>>>> 0936888989cb8315b6f35c285795976478616243
 end
 
